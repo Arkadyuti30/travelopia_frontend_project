2ea48723c0be768beb2a49ea4b20a53d37e45602
arkadyuti30 <arkadyutipaul@gmail.com>
2023-05-29 06:38:45 +0530

    created react app + main page

---
+++ src/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css';
+
+export default function MainPage() {
+	return(
+		<div id="main-page">
+			<div id="main-page-header">
+				<h2>Travel. Experience. Live.</h2>
+			</div>
+		</div>
+	)
+}
